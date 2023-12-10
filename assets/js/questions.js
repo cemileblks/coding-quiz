e362365b85818a71deaf3e28f89a1c3814b14be0
Cemileb <cemilebalkas@gmail.com>
2023-12-10 23:39:07 +0000

    added questions to js

---
+++ assets/js/questions.js
@@ -0,0 +1,22 @@
+const quizQuestions = [
+    {
+        quesion: 'What does JavaScript primarily add to a web page?',
+        choices: ['A) Style', 'B) Interactivity', 'C) Structure', 'D) Images'],
+        correctIndex: 1
+    },
+    {
+        quesion: 'What is the correct way to declare a variable in JavaScript?',
+        choices: ['A) let myVar = 10;', 'B) var myVar = 10;', 'C) const myVar = 10;', 'D) all of the above'],
+        correctIndex: 3
+    },
+    {
+        quesion: 'Which function is used to add a new element at the end of an array in JavaScript?',
+        choices: ['A) push()', 'B) pop()', 'C) shift()', 'D) unshift()'],
+        correctIndex: 0
+    },
+    {
+        quesion: 'Which of the following is NOT a valid JavaScript data type?',
+        choices: ['A) String', 'B) Object', 'C) Function', 'D) Character'],
+        correctIndex: 3
+    }
+  ];
