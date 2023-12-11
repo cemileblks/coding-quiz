805d515162eb498ef1198803bd81a792b0fe0139
Cemileb <cemilebalkas@gmail.com>
2023-12-11 00:23:23 +0000

    added functionality to start button

---
+++ assets/js/logic.js
@@ -0,0 +1,16 @@
+const startBtn = document.getElementById('start');
+const startScreen = document.getElementById('start-screen');
+
+
+
+
+
+
+
+
+
+startBtn.addEventListener('click', function(){
+    startBtn.classList.add('hide');
+    startScreen.classList.add('hide');
+})
+
